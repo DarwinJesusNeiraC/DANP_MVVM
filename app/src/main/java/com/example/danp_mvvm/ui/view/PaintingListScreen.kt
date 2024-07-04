2da68d7d0b163bbe0f6e7136b1b8b7efd7b7ed67
Paul_Luque <pluquec@unsa.edu.pe>
2024-07-04 16:53:24 -0500

    use hilt

--- app/src/main/java/com/example/danp_mvvm/ui/view/PaintingListScreen.kt
+++ app/src/main/java/com/example/danp_mvvm/ui/view/PaintingListScreen.kt
@@ -3,17 +3,18 @@ package com.example.danp_mvvm.ui.view
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.danp_mvvm.data.model.Painting
+import com.example.danp_mvvm.data.repository.PaintingRepository
 import com.example.danp_mvvm.ui.components.PaintingItem
 import com.example.danp_mvvm.ui.viewmodel.PaintingViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-@Composable
-fun PaintingListScreen(viewModel: PaintingViewModel = hiltViewModel()) {
-    val paintings by viewModel.paintings.observeAsState(emptyList())
-
-    LazyColumn {
-        items(paintings) { painting ->
-            PaintingItem(painting)
-        }
-    }
+@HiltViewModel
+class PaintingViewModel @Inject constructor(
+    private val repository: PaintingRepository // Suponiendo que tienes un repositorio
+) : ViewModel() {
+    val paintings: LiveData<List<Painting>> = repository.getPaintings()
 }
-
