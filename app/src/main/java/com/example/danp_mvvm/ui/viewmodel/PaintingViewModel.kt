325c3859e6e5c0d41cb48eba539bf89658d28001
Paul_Luque <pluquec@unsa.edu.pe>
2024-07-05 09:22:54 -0500

    Agrega un elemenot

--- app/src/main/java/com/example/danp_mvvm/ui/viewmodel/PaintingViewModel.kt
+++ app/src/main/java/com/example/danp_mvvm/ui/viewmodel/PaintingViewModel.kt
@@ -1,28 +1,10 @@
 package com.example.danp_mvvm.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.danp_mvvm.data.model.Painting
-import com.example.danp_mvvm.data.repository.PaintingRepository
+import androidx.lifecycle.*
 import com.example.danp_mvvm.domain.model.PaintingDomainModel
-import com.example.danp_mvvm.domain.repository.PaintingRepositoryImpl
 import com.example.danp_mvvm.domain.usecase.GetPaintingsUseCase
 import kotlinx.coroutines.launch
 
-//class PaintingViewModel(private val getPaintingsUseCase: GetPaintingsUseCase) : ViewModel() {
-//    private val _paintings = MutableLiveData<List<PaintingDomainModel>>()
-//    val paintings: LiveData<List<PaintingDomainModel>> = _paintings
-//
-//    init {
-//        viewModelScope.launch {
-//            _paintings.value = getPaintingsUseCase.execute()
-//        }
-//    }
-//}
-
-//@HiltViewModel
 class PaintingViewModel(private val getPaintingsUseCase: GetPaintingsUseCase) : ViewModel() {
     private val _paintings = MutableLiveData<List<PaintingDomainModel>>()
     val paintings: LiveData<List<PaintingDomainModel>> get() = _paintings
@@ -31,5 +13,23 @@ class PaintingViewModel(private val getPaintingsUseCase: GetPaintingsUseCase) : 
         viewModelScope.launch {
             _paintings.value = getPaintingsUseCase.execute()
         }
+    }
+
+    // Método para manejar el evento de agregar pintura
+    fun addPainting(painting: PaintingDomainModel) {
+        val updatedList = _paintings.value.orEmpty().toMutableList().apply {
+            add(painting)
+        }
+        _paintings.value = updatedList
+    }
+}
+
+class PaintingViewModelFactory(private val getPaintingsUseCase: GetPaintingsUseCase) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PaintingViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return PaintingViewModel(getPaintingsUseCase) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
